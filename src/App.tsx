6d55c22619d7aab37fcac630b4fe9125c473fd52
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-11-11 16:35:17 -0300

    Feat Development: Files update to jsx so I can work with React

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import AppRoot from "./components/shared/includes/AppRoot";
+import AllProducts from "./components/customer/products/AllProducts";
+import Signup from "./components/customer/auth/Signup";
+import Login from "./components/customer/auth/Login";
+import ProdcutDetails from "./components/customer/products/ProductDetails";
+import AllOrders from "./components/customer/orders/AllOrders";
+import AdminOrders from "./components/admin/orders/AdminOrders";
+import AdminProducts from "./components/admin/products/AdminProducts";
+import Cart from "./components/customer/cart/Cart";
+
 import "./less/base.less";
 import "./less/auth.less";
 import "./less/carousel.less";
@@ -8,21 +21,23 @@ import "./less/orders.less";
 import "./less/products.less";
 
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AllProducts from "./components/customer/products/AllProducts";
-import React from "react";
-import AppRoot from "./components/shared/includes/AppRoot";
-import Signup from "./components/customer/auth/Signup";
-import Login from "./components/customer/auth/Login";
-
 const router = createBrowserRouter([
   {
     path: "/",
     element: <AppRoot />,
     children: [
       {
-        path: "products",
+        index: true,
         element: <AllProducts />,
+      },
+      {
+        path: "products",
+        children: [
+          {
+            path: ":id",
+            element: <ProdcutDetails />
+          }
+        ],
       },
       {
         path: "signup",
@@ -31,6 +46,33 @@ const router = createBrowserRouter([
       {
         path: "login",
         element: <Login />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+      {
+        path: "orders",
+        element: <AllOrders />,
+      },
+      {
+        path: "admin",
+        children: [
+          {
+            path: "products",
+            element: <AdminProducts />,
+            children: [
+              {
+                path: ":id",
+                element: <ProdcutDetails />
+              }
+            ]
+          },
+          {
+            path: "orders",
+            element: <AdminOrders />
+          }
+        ],
       },
     ]
   }
--- src/components/customer/products/AllProducts.tsx
+++ src/components/customer/products/AllProducts.tsx
@@ -1,20 +1,48 @@
 // import "../../../less/products.less";
 // import "../../../less/carousel.less";
 
+import ProductItem from "../../shared/includes/ProductItem";
 
 export default function AllProducts() {
+
+  const products = [
+    {
+      title: "Titulo 1",
+      imageUrl: "testeimage.png",
+      id: "1"
+    }, {
+      title: "Titulo 2",
+      imageUrl: "testeimage.png",
+      id: "2"
+    }, {
+      title: "Titulo 3",
+      imageUrl: "testeimage.png",
+      id: "3"
+    }, {
+      title: "Titulo 4",
+      imageUrl: "testeimage.png",
+      id: "4"
+    }, {
+      title: "Titulo 5",
+      imageUrl: "testeimage.png",
+      id: "5"
+    }, {
+      title: "Titulo 6",
+      imageUrl: "testeimage.png",
+      id: "6"
+    }
+  ];
 
   return (
     <>
       <main>
         <h1>All Products</h1>
         <ul id="products-grid">
-          {/* <!-- <% for (const product of products) { %> --> */}
-          <li>
-            {/* Products
-          <!-- <%- include('../../shared/includes/product-item', { product: product }) %> --> */}
-          </li>
-          {/* <!-- <% } %> --> */}
+          {products.map(product => (
+            <li>
+              <ProductItem product={product} />
+            </li>
+          ))}
         </ul>
       </main>
     </>
