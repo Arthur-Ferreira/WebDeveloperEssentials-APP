9ee17b53fb1638a4a13a2a64614bd95c1efdde29
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-11-13 21:18:30 -0300

    Feat Development: Add rest and spred operators to forward multiple properties easily

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import SButton from "./styles";
 
 interface IButton {
-    children: React.ReactNode | string;
-    $alt?: boolean;
-    type?: "button" | "reset" | "submit";
-    as?: any;
-    to?: string;
+  children: React.ReactNode | string;
+  $alt?: boolean;
+  type?: "button" | "reset" | "submit";
+  as?: any;
+  to?: string;
 }
 
-const Button = ({ children, $alt, type, as, to }: IButton): JSX.Element => {
-    return <SButton $alt={$alt} type={type} as={as} to={to}>{children}</SButton>
+const Button = ({ children, ...props }: IButton): JSX.Element => {
+  return <SButton {...props} >{children}</SButton>
 }
 
 
