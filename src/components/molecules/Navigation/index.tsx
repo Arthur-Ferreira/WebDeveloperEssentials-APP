8952c497fdf7167a05cb7bdae8f81ce4cef3cde0
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-11-21 12:40:07 -0300

    Feat Development: Remove Orders from menu

--- src/components/molecules/Navigation/index.tsx
+++ src/components/molecules/Navigation/index.tsx
@@ -9,21 +9,20 @@ const Navigation: React.FC = () => {
       <NavItems>
         <li><Anchor to="/">Shop</Anchor></li>
         <li>
-          <Anchor to="/cart">
-            Cart
-            <Badge>1</Badge>
-          </Anchor>
+          <Anchor to="/cart">Cart <Badge>1</Badge></Anchor>
         </li>
-        <li><Anchor to="/orders">Orders</Anchor></li>
-        {/* <li><Anchor to="/admin/products">Manage Products</Anchor></li>
-        <li><Anchor to="/admin/orders">Manage Orders</Anchor></li> */}
+        {/* 
+          <li><Anchor to="/orders">Orders</Anchor></li>
+          <li><Anchor to="/admin/products">Manage Products</Anchor></li>
+          <li><Anchor to="/admin/orders">Manage Orders</Anchor></li> 
+        */}
         <li><Anchor to="/signup">Signup</Anchor></li>
         <li><Anchor to="/login">Login</Anchor></li>
         {/* <li>
-          <form action="/logout" method="POST">
+          <Form action="/logout" method="POST">
             // <input type="hidden" name="_csrf" value="<%= locals.csrfToken %>" />
             <Button>Logout</Button>
-          </form>
+          </Form>
         </li> */}
       </NavItems>
     </>
