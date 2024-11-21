ad8474fbe27418113cee9efda9ba8d445bdeaed7
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-11-21 12:41:13 -0300

    Feat Development: Create forms Label component

---
+++ src/components/atoms/Label/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { colors, spacing } from "../../../global/vars";
+
+const SLabel = styled.label`
+  color: ${colors.gray100};
+  display: block;
+  margin-bottom: ${spacing.space2};
+`;
+
+export default SLabel
