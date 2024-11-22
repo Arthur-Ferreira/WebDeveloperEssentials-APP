ab611e2c2fc6871b0902037165c93c8e1fae028b
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-11-22 14:40:40 -0300

    Feat Development: Create App Types

---
+++ src/types/index.d.ts
@@ -0,0 +1,74 @@
+import { ObjectId } from "mongodb";
+
+export { }
+
+declare global {
+
+  interface IProduct {
+    title: string;
+    summary: string;
+    price: number;
+    description: string;
+    image: string;
+    imagePath: string;
+    imageUrl: string;
+    _id: ObjectId | { $oid: string }
+  }
+
+  interface IAddress {
+    street: string
+    postalCode: string
+    city: string
+  }
+
+  interface IUser {
+    _id: ObjectId | { $oid: string }
+    email: string
+    password: string
+    name: string
+    address: IAddress
+    isAdmin?: boolean
+  }
+
+  interface ISession {
+    cookie: Object
+    csrfSecret: string
+    flashedData: Object
+    uid: string
+    isAdmin: boolean
+  }
+
+  interface IAuth {
+    _id: ObjectId | { $oid: string }
+    expires: Date | { $date: { $numberLong: string; }; }
+    session: ISession
+  }
+
+  interface IUserData {
+    _id: ObjectId | { $oid: string }
+    email: string
+    name: string
+    address: IAddress
+  }
+
+  interface ItemsProducts {
+    product: IProduct
+    quantity: number
+    totalPrice: number | { $numberInt: string; }
+  }
+
+  interface IProductData {
+    items: IItemsProducts[]
+    totalQuantity: number | { $numberInt: string; }
+    totalPrice: number | { $numberInt: string; }
+  }
+
+  interface IOrder {
+    _id: ObjectId | { $oid: string }
+    userData: IUserData
+    productData: IProductData
+    date: Date | { $date: { $numberLong: string; }; }
+    status: string | "pending" | "fullfiled" | "cancelled"
+  }
+
+}
