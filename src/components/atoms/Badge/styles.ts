64179e65d5c427d0374978133e395fcadee1473a
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-11-30 12:50:54 -0300

    Feat Development: Bug fix at Badge styles

--- src/components/atoms/Badge/styles.ts
+++ src/components/atoms/Badge/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { colors, spacing } from "../../../global/vars";
 
 const SBadge = styled.span`
-  display: inline-block
+  display: inline-block;
   margin-left: ${spacing.space2};
   padding: 0.15rem ${spacing.space4};
   border-radius: 10rem;
