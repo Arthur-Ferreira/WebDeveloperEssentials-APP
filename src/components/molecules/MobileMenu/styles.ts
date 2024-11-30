966fe7d6159f8edb86e09786e7f1e3670cc38ca5
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-11-30 15:38:27 -0300

    Feat Development:R Remove css from styled-component of this file

--- src/components/molecules/MobileMenu/styles.ts
+++ src/components/molecules/MobileMenu/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { breakpoints, colors } from "../../../global/vars";
 
 const SMobileMenu = styled.aside`
