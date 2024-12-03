e656c70db5397d8a4b773792a90257ab811ccae4
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-12-03 17:49:23 -0300

    Feat Typescript: Create AllProductsLoader to featch data from an API and pass to AllProducts component at products route

---
+++ src/utils/loaders.ts
@@ -0,0 +1,19 @@
+import { json } from "react-router-dom";
+
+export async function allProductsLoader(): Promise<IProduct> {
+  let url = "http://localhost:3031"
+  try {
+
+    const response = await fetch(`${url}/products`)
+
+    if (!response.ok) {
+      throw json({ message: "Error on fetching all Products!" }, { status: 500 })
+    } else {
+      return response.json() as Promise<IProduct>
+    }
+  } catch (err) {
+    throw err
+  }
+
+}
+
