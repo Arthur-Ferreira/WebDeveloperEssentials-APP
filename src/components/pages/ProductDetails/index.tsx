7cc76cf2a89f342cb330fae73f34d869b3941ea6
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-12-24 12:33:31 -0300

    Feat Typescript: Remove boilerplate and add loader data

--- src/components/pages/ProductDetails/index.tsx
+++ src/components/pages/ProductDetails/index.tsx
@@ -14,7 +14,7 @@ const ProdcutDetails: React.FC = () => {
   return (
     <main>
       <ProductDetailsHeader>
-        <ProductDetailsImage src={product.imagePath} alt={`${product.title}`} />
+        <ProductDetailsImage src={product.imageUrl} alt={`${product.title}`} />
         <ProductDetailsInfo>
           <H2>
             {product.title}
@@ -31,11 +31,6 @@ const ProdcutDetails: React.FC = () => {
 
       <ProductDetailsDescription>
         {product.description}
-
-        {/* Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, officiis nam. A unde quia aut adipisci odio est iusto facilis ipsa maiores possimus dignissimos quae fugiat, sed omnis nisi quasi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, exercitationem voluptas commodi, odio fuga quis, quas possimus optio aliquam soluta ex eos labore numquam molestiae! Et autem unde deserunt hic?
-
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, officiis nam. A unde quia aut adipisci odio est iusto facilis ipsa maiores possimus dignissimos quae fugiat, sed omnis nisi quasi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, exercitationem voluptas commodi, odio fuga quis, quas possimus optio aliquam soluta ex eos labore numquam molestiae! Et autem unde deserunt hic?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, officiis nam. A unde quia aut adipisci odio est iusto facilis ipsa maiores possimus dignissimos quae fugiat, sed omnis nisi quasi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, exercitationem voluptas commodi, odio fuga quis, quas possimus optio aliquam soluta ex eos labore numquam molestiae! Et autem unde deserunt hic? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore architecto amet ipsam provident totam. Harum dignissimos est modi sunt eveniet? Quos ea provident libero debitis accusantium minus non officia nobis! */}
       </ProductDetailsDescription>
     </main>
   );
